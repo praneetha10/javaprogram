3f8c4aed5d3f0f57203d494120c7269a75cc3985
praneetha10 <praneetha.mullapudi@gmail.com>
2021-07-22 12:55:25 +0530

    inherentance concept

---
+++ Javaprograms/src/test/java/Javaprograms/test1.java
@@ -0,0 +1,37 @@
+package Javaprograms;
+
+ class test2
+{
+	public void m1(String x,int y,float f)
+	{
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(f);
+	}
+	public void m1(int x,float y,String z)
+	{
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+}
+ public class test1 extends test2
+{
+	public void m1(int x,float y,String z)
+	{
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+	public static void main(String[] args)
+	{
+		test2 t2=new test2();
+		t2.m1(20, (float)10.2, "praneetha");
+	}
+
+}
+
+
+
+	
+
