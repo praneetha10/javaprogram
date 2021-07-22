e0422fcd7fd56dfa37dd6da7eeac5a05d2ca643a
praneetha10 <65086031+praneetha10@users.noreply.github.com>
2021-07-22 13:02:14 +0530

    Update test1.java

--- Javaprograms/src/test/java/Javaprograms/test1.java
+++ Javaprograms/src/test/java/Javaprograms/test1.java
@@ -14,6 +14,11 @@ package Javaprograms;
 		System.out.println(y);
 		System.out.println(z);
 	}
+	 public String m2(String x,int a)
+	 {
+		 System.out.println(x);
+		 return x
+	 }
 }
  public class test1 extends test2
 {
@@ -27,6 +32,7 @@ package Javaprograms;
 	{
 		test2 t2=new test2();
 		t2.m1(20, (float)10.2, "praneetha");
+		t2.m2("praneetha",10);
 	}
 
 }
